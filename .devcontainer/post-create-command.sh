f35b22efc515ec658e7e59369cb887847dc033f2
Conquistadore <info@flexforge.me>
2022-12-02 16:18:29 +0100

    Improve container/debugging experience

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -4,3 +4,4 @@ git config --global --add safe.directory $1
 cd $1
 npm install
 npm link
+echo "$(jq '. += {"platforms":[{"platform":"JVCTV"}]}' ~/.homebridge/config.json)" > ~/.homebridge/config.json
